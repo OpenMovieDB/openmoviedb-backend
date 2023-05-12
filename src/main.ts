c46e17146cacc9171122432dac89808e17ed5043
mdwit <mdwit0r@gmail.com>
2023-05-12 16:23:28 +0200

    feat(main.ts): add fastify adapter setup with helmet and gzip compression feat(main.ts): add cors and global pipes and interceptors feat(status-app-message.util.ts): add status app message util to log app url and env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe, VersioningType } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestFastifyApplication } from '@nestjs/platform-fastify';
+import { setupFastify } from './fastify';
+import { statusAppMessage } from './common/utils/status-app-message.util';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const { PORT } = process.env;
+
+  const adapter = setupFastify();
+
+  const app = await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      adapter,
+      {
+        bufferLogs: true,
+      },
+  );
+
+  app.enableCors({ origin: '*' });
+
+  app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+      }),
+  );
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+
+  await app.listen(PORT || 3000, '0.0.0.0');
+
+  await statusAppMessage(app);
 }
+
 bootstrap();
