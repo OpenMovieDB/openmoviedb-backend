f15775a2d036267ea57835e6c336e5d24bc5e499
mdwit <mdwit0r@gmail.com>
2023-05-21 01:50:48 +0200

    refactor(schema): remove 'type' field from MediaAsset model
    The 'type' field was removed from the MediaAsset model in order to simplify the schema and remove unnecessary fields. The 'type' field was dropped from the MediaAsset table in the migration file '20230520230427_' to ensure that all data in the column was lost.

---
+++ prisma/migrations/20230520230427_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `type` on the `MediaAsset` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "MediaAsset" DROP COLUMN "type";
