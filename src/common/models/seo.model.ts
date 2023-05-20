6015615f11a35f916cb81e1efe69c93d6541ecf5
mdwit <mdwit0r@gmail.com>
2023-05-20 20:02:46 +0200

    feat: add seo model

---
+++ src/common/models/seo.model.ts
@@ -0,0 +1,30 @@
+import { Field, ObjectType, registerEnumType } from '@nestjs/graphql';
+import { BaseModel } from './base.model';
+import { ImageModel } from './image.model';
+
+enum SeoType {
+  OPEN_GRAPH,
+  TWITTER_CARD,
+  JSONLD,
+  BASIC,
+}
+
+registerEnumType(SeoType, {
+  name: 'SeoType',
+  description: 'Type of SEO',
+});
+
+@ObjectType()
+export class SeoModel extends BaseModel {
+  @Field({ nullable: true })
+  title?: string | null;
+
+  @Field({ nullable: true })
+  description?: string | null;
+
+  @Field((type) => ImageModel, { nullable: true })
+  image?: ImageModel | null;
+
+  @Field((type) => SeoType)
+  type: SeoType;
+}
