2beb4121296adb3b2f390d5e02148d2d6a1f6925
mdwit <mdwit0r@gmail.com>
2023-05-24 21:54:32 +0200

    fix(genre.model.ts): fix import path for ImageModel in genre.model.ts

--- src/genre/models/genre.model.ts
+++ src/genre/models/genre.model.ts
@@ -1,6 +1,6 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { BasePageModel } from '../../common/models/base-page.model';
-import { ImageModel } from '../../common/models/image.model';
+import { ImageModel } from '../../image/models/image.model';
 import { MovieModel } from '../../movie/models/movie.model';
 
 @ObjectType()
