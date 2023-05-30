272002265c805df5e19602a8d89018c32de2ff14
mdwit <mdwit0r@gmail.com>
2023-05-30 20:11:30 +0200

    feat(find-movies.input.ts): add year and rating fields to FindMoviesInput class for filtering movies by year and rating. Add NumberFilterInput and RatingRelationInput classes for filtering by number and rating.

--- src/movie/dto/find-movies.input.ts
+++ src/movie/dto/find-movies.input.ts
@@ -1,6 +1,8 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { NumberFilterInput } from 'src/common/dto/number-filter.input';
 import { StringFilterInput } from 'src/common/dto/string-filter.input';
 import { UuidFilterInput } from 'src/common/dto/uuid-filter.input';
+import { RatingRelationInput } from './rating.input';
 
 @InputType()
 export class FindMoviesInput {
@@ -12,4 +14,10 @@ export class FindMoviesInput {
 
   @Field({ nullable: true })
   slug?: StringFilterInput;
+
+  @Field({ nullable: true })
+  year?: NumberFilterInput;
+
+  @Field({ nullable: true })
+  rating?: RatingRelationInput;
 }
