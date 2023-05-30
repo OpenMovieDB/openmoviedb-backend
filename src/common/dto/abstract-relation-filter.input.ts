635e6728d38858f33787a09bdf6b5e809e7ec242
mdwit <mdwit0r@gmail.com>
2023-05-30 20:08:07 +0200

    feat(common): add AbstractRelationFilterInput function to create abstract relation filter input types for GraphQL resolvers

---
+++ src/common/dto/abstract-relation-filter.input.ts
@@ -0,0 +1,17 @@
+import { InputType, Field } from '@nestjs/graphql';
+
+export function AbstractRelationFilterInput<T>(TClass: new () => T): any {
+  @InputType({ isAbstract: true })
+  abstract class AbstractRelationInput {
+    @Field(() => TClass, { nullable: true })
+    abstract every?: T;
+
+    @Field(() => TClass, { nullable: true })
+    abstract some?: T;
+
+    @Field(() => TClass, { nullable: true })
+    abstract none?: T;
+  }
+
+  return AbstractRelationInput;
+}
