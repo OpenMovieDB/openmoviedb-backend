c890cf5503426308dec15c66d934888982dfa4ca
mdwit <mdwit0r@gmail.com>
2023-06-03 16:58:14 +0200

    feat(docker-compose.yml): add minio service to docker-compose file feat(image-asset.model.ts): change ImageAssetFormat and ImageAssetWidth enums to string values

--- src/image/models/image-asset.model.ts
+++ src/image/models/image-asset.model.ts
@@ -2,22 +2,22 @@ import { Field, ObjectType, registerEnumType } from '@nestjs/graphql';
 import { BaseModel } from '../../common/models/base.model';
 
 export enum ImageAssetFormat {
-  AVIF,
-  GIF,
-  JPEG,
-  PNG,
-  WEBP,
+  AVI = 'AVIF',
+  GIF = 'GIF',
+  JPEG = 'JPEG',
+  PNG = 'PNG',
+  WEBP = 'WEBP',
 }
 
 export enum ImageAssetWidth {
-  W375,
-  W768,
-  W1024,
-  W1280,
-  W1366,
-  W1600,
-  W1920,
-  W2560,
+  W375 = 'W375',
+  W768 = 'W768',
+  W1024 = 'W1024',
+  W1280 = 'W1280',
+  W1366 = 'W1366',
+  W1600 = 'W1600',
+  W1920 = 'W1920',
+  W2560 = 'W2560',
 }
 
 registerEnumType(ImageAssetFormat, {
