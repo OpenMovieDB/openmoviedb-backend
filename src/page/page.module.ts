1b008028aa1773de279324d2aa01c1e43405b771
mdwit <mdwit0r@gmail.com>
2023-06-10 23:09:11 +0200

    feat: add support for image links and block id in CollectionModel and BlockModel feat: add BlockModule, PageInfoModule, and ImageModule to CollectionModule and PageModule feat: add BlocksLoader and CollectionsLoader to BlockModule and CollectionModule respectively

--- src/page/page.module.ts
+++ src/page/page.module.ts
@@ -3,9 +3,10 @@ import { PageResolver } from './page.resolver';
 import { PageService } from './page.service';
 import PagesLoader from './page.loader';
 import { PageInfoModule } from 'src/page-info/page-info.module';
+import { BlockModule } from 'src/block/block.module';
 
 @Module({
-  imports: [PageInfoModule],
+  imports: [PageInfoModule, BlockModule],
   providers: [PageResolver, PageService, PagesLoader],
 })
 export class PageModule {}
