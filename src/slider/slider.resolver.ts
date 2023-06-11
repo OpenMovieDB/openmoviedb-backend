91cd7dcb516ae62418eb99ba169a84a7f31c4ad4
mdwit <mdwit0r@gmail.com>
2023-06-11 03:17:32 +0200

    feat(slider): add SliderLoader to batch load slides for a slider feat(slider): add findOne and findMany methods to SliderService feat(slider): add SliderResolver to handle queries for sliders and slides feat(slider): add SliderMapper to map Slider entities to Slider models

--- src/slider/slider.resolver.ts
+++ src/slider/slider.resolver.ts
@@ -1,4 +1,28 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { SliderModel } from './models/slider.model';
+import SlidersLoader from './slider.loader';
+import { SliderService } from './slider.service';
+import { PaginationArgs } from 'src/common/pagination/pagination.args';
+import { FindSlidersInput } from './dto/find-slider.input';
+import { SlidersModel } from './models/sliders.model';
+import { SlideModel } from 'src/slide/models/slide.model';
 
-@Resolver()
-export class SliderResolver {}
+@Resolver(() => SliderModel)
+export class SliderResolver {
+  constructor(private readonly slidersLoader: SlidersLoader, private readonly sliderService: SliderService) {}
+
+  @Query(() => SliderModel)
+  async slider(@Args('id') id: string): Promise<SliderModel> {
+    return this.sliderService.findOne(id);
+  }
+
+  @Query(() => [SlidersModel])
+  async sliders(@Args() pagination: PaginationArgs, @Args('data') dto: FindSlidersInput): Promise<SlidersModel> {
+    return this.sliderService.findMany(pagination, dto);
+  }
+
+  @ResolveField('slides', () => [SlideModel], { nullable: true })
+  async sliderSlides(@Parent() slider: SliderModel): Promise<SlideModel[]> {
+    return this.slidersLoader.batchSlides.load(slider.id);
+  }
+}
--- src/slider/slider.service.ts
+++ src/slider/slider.service.ts
@@ -1,7 +1,37 @@
+import { findManyCursorConnection } from '@devoxa/prisma-relay-cursor-connection';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'nestjs-prisma';
+import { PaginationArgs } from 'src/common/pagination/pagination.args';
+import { SliderModel } from './models/slider.model';
+import { SliderMapper } from './slider.mapper';
+import { FindSlidersInput } from './dto/find-slider.input';
+import { SlidersModel } from './models/sliders.model';
 
 @Injectable()
 export class SliderService {
   constructor(private readonly prismaService: PrismaService) {}
+
+  async findOne(id: string): Promise<SliderModel> {
+    const slider = await this.prismaService.slider.findUnique({
+      where: { id },
+    });
+
+    return new SliderMapper().mapEntityToModel(slider);
+  }
+
+  async findMany({ after, before, first, last }: PaginationArgs, where: FindSlidersInput): Promise<SlidersModel> {
+    const res = await findManyCursorConnection(
+      (args) => this.prismaService.slider.findMany({ where }),
+      () => this.prismaService.slider.count({ where }),
+      { after, before, first, last },
+    );
+
+    return {
+      ...res,
+      edges: res.edges.map((edge) => ({
+        ...edge,
+        node: new SliderMapper().mapEntityToModel(edge.node),
+      })),
+    };
+  }
 }
