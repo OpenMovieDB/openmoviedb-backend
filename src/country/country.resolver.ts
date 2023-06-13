a9ad158938f3a7de310e1aa2b176d814583bea3c
mdwit <mdwit0r@gmail.com>
2023-06-13 22:05:06 +0200

    refactor(country.resolver.ts, media.resolver.ts, schema.graphql): remove unused code and queries

--- src/country/country.resolver.ts
+++ src/country/country.resolver.ts
@@ -1,5 +1,4 @@
-import { Args, Mutation, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
-import { PaginationArgs } from 'src/common/pagination/pagination.args';
+import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
 import { ImageLinkModel } from 'src/image/models/image-link.model';
 import { CountryService } from './country.service';
 import { CountriesModel } from './models/countries.model';
@@ -7,7 +6,6 @@ import { CountryModel } from './models/country.model';
 import { FindCountriesInput } from './dto/find-countries.input';
 import CountryLoader from './country.loader';
 import { CreateCountryInput } from './dto/create-country.input';
-import { CreateCountriesInput } from './dto/create-countries.input';
 import { BaseResolver } from '../common/resolvers/base.resolver';
 import { UpdateCountryInput } from './dto/update-country.input';
 
@@ -23,20 +21,6 @@ export class CountryResolver extends BaseResolver(
 ) {
   constructor(private readonly countryService: CountryService, private readonly countryLoader: CountryLoader) {
     super(countryService);
-  }
-  @Mutation(() => [CountryModel])
-  async createCountries(@Args('data') data: CreateCountriesInput): Promise<CountryModel[]> {
-    return this.countryService.createMany(data);
-  }
-
-  @Query(() => CountryModel)
-  async country(@Args('id') id: string): Promise<CountryModel> {
-    return this.countryService.findOne(id);
-  }
-
-  @Query(() => CountriesModel)
-  async countries(@Args() pagination: PaginationArgs, @Args('data') dto: FindCountriesInput): Promise<CountriesModel> {
-    return this.countryService.findMany(pagination, dto);
   }
 
   @ResolveField('images', () => [ImageLinkModel], { nullable: true })
--- src/media/media.resolver.ts
+++ src/media/media.resolver.ts
@@ -1,6 +1,5 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Resolver } from '@nestjs/graphql';
 import { MediaService } from './media.service';
-import { MediaLinkModel } from './models/media-link.model';
 import { BaseResolver } from '../common/resolvers/base.resolver';
 import { MediaModel } from './models/media.model';
 import { CreateMediaInput } from './dto/create-media.input';
@@ -20,10 +19,5 @@ export class MediaResolver extends BaseResolver(
 ) {
   constructor(private readonly mediaService: MediaService) {
     super(mediaService);
-  }
-
-  @Query(() => MediaLinkModel)
-  async media(@Args('id') id: string): Promise<MediaLinkModel> {
-    return this.mediaService.findOne(id);
   }
 }
