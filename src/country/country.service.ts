35247331896503144a26a6c5572b90c17bddf0c9
mdwit <mdwit0r@gmail.com>
2023-06-13 21:58:31 +0200

    refactor(country.service.ts): extend BaseService class to reduce code duplication and improve readability

--- src/country/country.service.ts
+++ src/country/country.service.ts
@@ -1,53 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'nestjs-prisma';
 import { CountryModel } from './models/country.model';
-import { CountryMapper } from './country.mapper';
-import { findManyCursorConnection } from '@devoxa/prisma-relay-cursor-connection';
-import { PaginationArgs } from 'src/common/pagination/pagination.args';
 import { FindCountriesInput } from './dto/find-countries.input';
 import { CountriesModel } from './models/countries.model';
 import { CreateCountriesInput } from './dto/create-countries.input';
+import { BaseService } from '../common/services/base.service';
+import { CreateCountryInput } from './dto/create-country.input';
+import { CountryMapper } from './country.mapper';
 
 @Injectable()
-export class CountryService {
-  constructor(private readonly prismaService: PrismaService) {}
-
-  async findOne(id: string): Promise<CountryModel> {
-    const country = await this.prismaService.country.findUnique({
-      where: { id },
-    });
-
-    return new CountryMapper().mapEntityToModel(country);
-  }
-
-  async findMany({ after, before, first, last }: PaginationArgs, dto: FindCountriesInput): Promise<CountriesModel> {
-    const res = await findManyCursorConnection(
-      (args) =>
-        this.prismaService.country.findMany({
-          where: {
-            title: {
-              contains: dto.title,
-            },
-          },
-        }),
-      () =>
-        this.prismaService.country.count({
-          where: {
-            title: {
-              contains: dto.title,
-            },
-          },
-        }),
-      { after, before, first, last },
-    );
-
-    return {
-      ...res,
-      edges: res.edges.map((edge) => ({
-        ...edge,
-        node: new CountryMapper().mapEntityToModel(edge.node),
-      })),
-    };
+export class CountryService extends BaseService(
+  'country',
+  CountryModel,
+  CountryModel,
+  CountriesModel,
+  FindCountriesInput,
+  CreateCountryInput,
+  CountryMapper,
+) {
+  constructor(readonly prismaService: PrismaService) {
+    super(prismaService);
   }
 
   async findManyByMovieIds(ids: string[]): Promise<{ movieId: string; countries: CountryModel[] }[]> {
