8bd45f3f12896133356cebe7a2c8428da1b04bc6
mdwit <mdwit0r@gmail.com>
2023-06-13 21:59:57 +0200

    refactor(movie.service.ts): extend BaseService class to reduce code duplication and improve readability

--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -3,37 +3,22 @@ import { PrismaService } from 'nestjs-prisma';
 import { MovieModel } from './models/movie.model';
 import { MovieMapper } from './movie.mapper';
 import { FindMoviesInput } from './dto/find-movies.input';
-import { PaginationArgs } from 'src/common/pagination/pagination.args';
-import { findManyCursorConnection } from '@devoxa/prisma-relay-cursor-connection';
 import { MoviesModel } from './models/movies.model';
 import { CreateMovieInput } from './dto/create-movie.input';
+import { BaseService } from '../common/services/base.service';
 
 @Injectable()
-export class MovieService {
-  constructor(private readonly prismaService: PrismaService) {}
-
-  async findOne(id: string): Promise<MovieModel> {
-    const movie = await this.prismaService.movie.findUnique({
-      where: { id },
-    });
-
-    return new MovieMapper().mapEntityToModel(movie);
-  }
-
-  async findMany({ after, before, first, last }: PaginationArgs, where: FindMoviesInput): Promise<MoviesModel> {
-    const res = await findManyCursorConnection(
-      (args) => this.prismaService.movie.findMany({ where }),
-      () => this.prismaService.movie.count({ where }),
-      { after, before, first, last },
-    );
-
-    return {
-      ...res,
-      edges: res.edges.map((edge) => ({
-        ...edge,
-        node: new MovieMapper().mapEntityToModel(edge.node),
-      })),
-    };
+export class MovieService extends BaseService(
+  'movie',
+  MovieModel,
+  MovieModel,
+  MoviesModel,
+  FindMoviesInput,
+  CreateMovieInput,
+  MovieMapper,
+) {
+  constructor(readonly prismaService: PrismaService) {
+    super(prismaService);
   }
 
   async findManyByIds(ids: string[]): Promise<MovieModel[]> {
@@ -46,26 +31,5 @@ export class MovieService {
     });
 
     return movies.map((movie) => new MovieMapper().mapEntityToModel(movie));
-  }
-
-  async create(data: CreateMovieInput): Promise<MovieModel> {
-    const movie = await this.prismaService.movie.create({
-      data: {
-        ...data,
-        rating: {
-          create: {
-            value: null,
-          },
-        },
-        pageInfo: {
-          create: {
-            description: null,
-            title: null,
-          },
-        },
-      },
-    });
-
-    return new MovieMapper().mapEntityToModel(movie);
   }
 }
