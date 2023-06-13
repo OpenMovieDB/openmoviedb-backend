2f0703f743ccaa2695882aba8b2664b29aaf54a3
mdwit <mdwit0r@gmail.com>
2023-06-13 21:58:05 +0200

    refactor(block.service.ts): extend BaseService class to reduce code duplication and improve readability

--- src/block/block.service.ts
+++ src/block/block.service.ts
@@ -1,40 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'nestjs-prisma';
-
-import { PaginationArgs } from '../common/pagination/pagination.args';
 import { FindBlocksInput } from './dto/find-blocks.input';
 import { BlockMapper } from './block.mapper';
 import { CreateBlockInput } from './dto/create-block.input';
-import { findManyCursorConnection } from '@devoxa/prisma-relay-cursor-connection';
 import { BlocksModel } from './models/blocks.model';
 import { BlockModel } from './models/block.model';
+import { BaseService } from '../common/services/base.service';
 
 @Injectable()
-export class BlockService {
-  constructor(private readonly prismaService: PrismaService) {}
-
-  async findOne(id: string): Promise<BlockModel> {
-    const block = await this.prismaService.block.findUnique({
-      where: { id },
-    });
-
-    return new BlockMapper().mapEntityToModel(block);
-  }
-
-  async findMany({ after, before, first, last }: PaginationArgs, where: FindBlocksInput): Promise<BlocksModel> {
-    const res = await findManyCursorConnection(
-      (args) => this.prismaService.block.findMany({ where }),
-      () => this.prismaService.block.count({ where }),
-      { after, before, first, last },
-    );
-
-    return {
-      ...res,
-      edges: res.edges.map((edge) => ({
-        ...edge,
-        node: new BlockMapper().mapEntityToModel(edge.node),
-      })),
-    };
+export class BlockService extends BaseService(
+  'block',
+  BlockModel,
+  BlockModel,
+  BlocksModel,
+  FindBlocksInput,
+  CreateBlockInput,
+  BlockMapper,
+) {
+  constructor(readonly prismaService: PrismaService) {
+    super(prismaService);
   }
 
   async getBlockByIds(ids: string[]): Promise<BlockModel[]> {
@@ -58,26 +42,5 @@ export class BlockService {
     });
 
     return new BlockMapper().mapEntitiesToModels(blocks);
-  }
-
-  async create(data: CreateBlockInput): Promise<BlockModel> {
-    const block = await this.prismaService.block.create({
-      data: {
-        ...data,
-      },
-    });
-
-    return new BlockMapper().mapEntityToModel(block);
-  }
-
-  async update(id: string, data: CreateBlockInput): Promise<BlockModel> {
-    const block = await this.prismaService.block.update({
-      where: { id },
-      data: {
-        ...data,
-      },
-    });
-
-    return new BlockMapper().mapEntityToModel(block);
   }
 }
