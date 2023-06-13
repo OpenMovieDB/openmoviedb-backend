e9b8fdcb5b09261fc7307f6c81946516efd3e2f7
mdwit <mdwit0r@gmail.com>
2023-06-13 22:01:21 +0200

    refactor(collection.service.ts): extend BaseService class to reduce code duplication and improve readability

--- src/collection/collection.service.ts
+++ src/collection/collection.service.ts
@@ -1,68 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import { PaginationArgs } from '../common/pagination/pagination.args';
 import { CollectionModel } from './models/collection.model';
 import { FindCollectionsInput } from './dto/find-collections.input';
 import { CollectionMapper } from './collection.mapper';
-import { findManyCursorConnection } from '@devoxa/prisma-relay-cursor-connection';
 import { CreateCollectionInput } from './dto/create-collection.input';
 import { PrismaService } from 'nestjs-prisma';
 import { CollectionsModel } from './models/collections.model';
+import { BaseService } from '../common/services/base.service';
 
 @Injectable()
-export class CollectionService {
-  constructor(private readonly prismaService: PrismaService) {}
-
-  async findOne(id: string): Promise<CollectionModel> {
-    const collection = await this.prismaService.collection.findUnique({
-      where: { id },
-    });
-
-    return new CollectionMapper().mapEntityToModel(collection);
-  }
-
-  async findMany(
-    { after, before, first, last }: PaginationArgs,
-    where: FindCollectionsInput,
-  ): Promise<CollectionsModel> {
-    const res = await findManyCursorConnection(
-      (args) => this.prismaService.collection.findMany({ where }),
-      () => this.prismaService.collection.count({ where }),
-      { after, before, first, last },
-    );
-
-    return {
-      ...res,
-      edges: res.edges.map((edge) => ({
-        ...edge,
-        node: new CollectionMapper().mapEntityToModel(edge.node),
-      })),
-    };
-  }
-
-  async create(data: CreateCollectionInput): Promise<CollectionModel> {
-    const collection = await this.prismaService.collection.create({
-      data: {
-        ...data,
-        pageInfo: {
-          create: {
-            title: null,
-            description: null,
-          },
-        },
-      },
-    });
-
-    return new CollectionMapper().mapEntityToModel(collection);
-  }
-
-  async update(id: string, data: CreateCollectionInput): Promise<CollectionModel> {
-    const collection = await this.prismaService.collection.update({
-      where: { id },
-      data: {
-        ...data,
-      },
-    });
-
-    return new CollectionMapper().mapEntityToModel(collection);
+export class CollectionService extends BaseService(
+  'collection',
+  CollectionModel,
+  CollectionModel,
+  CollectionsModel,
+  FindCollectionsInput,
+  CreateCollectionInput,
+  CollectionMapper,
+) {
+  constructor(readonly prismaService: PrismaService) {
+    super(prismaService);
   }
 }
