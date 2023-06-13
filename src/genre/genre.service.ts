3b33b54cf59b8eb6fca8a19c83179c1c4963d34e
mdwit <mdwit0r@gmail.com>
2023-06-13 22:01:48 +0200

    refactor(genre.service.ts): extend BaseService class to reduce code duplication and improve readability

--- src/genre/genre.service.ts
+++ src/genre/genre.service.ts
@@ -2,54 +2,24 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'nestjs-prisma';
 import { GenreModel } from './models/genre.model';
 import { GenreMapper } from './genre.mapper';
-import { ImageLinkMapper } from 'src/image/mappers/image-link.mapper';
-import { ImageLinkModel } from 'src/image/models/image-link.model';
-import { findManyCursorConnection } from '@devoxa/prisma-relay-cursor-connection';
-import { PaginationArgs } from 'src/common/pagination/pagination.args';
 import { FindGenresInput } from 'src/genre/dto/find-genres.input';
 import { GenresModel } from './models/genres.model';
 import { CreateGenresInput } from './dto/create-genres.input';
+import { BaseService } from '../common/services/base.service';
+import { CreateGenreInput } from './dto/create-genre.input';
 
 @Injectable()
-export class GenreService {
-  constructor(private readonly prismaService: PrismaService) {}
-
-  async findOne(id: string): Promise<GenreModel> {
-    const genre = await this.prismaService.genre.findUnique({
-      where: { id },
-    });
-
-    return new GenreMapper().mapEntityToModel(genre);
-  }
-
-  async findMany({ after, before, first, last }: PaginationArgs, dto: FindGenresInput): Promise<GenresModel> {
-    const res = await findManyCursorConnection(
-      (args) =>
-        this.prismaService.genre.findMany({
-          where: {
-            title: {
-              contains: dto.title,
-            },
-          },
-        }),
-      () =>
-        this.prismaService.genre.count({
-          where: {
-            title: {
-              contains: dto.title,
-            },
-          },
-        }),
-      { after, before, first, last },
-    );
-
-    return {
-      ...res,
-      edges: res.edges.map((edge) => ({
-        ...edge,
-        node: new GenreMapper().mapEntityToModel(edge.node),
-      })),
-    };
+export class GenreService extends BaseService(
+  'genre',
+  GenreModel,
+  GenreModel,
+  GenresModel,
+  FindGenresInput,
+  CreateGenreInput,
+  GenreMapper,
+) {
+  constructor(readonly prismaService: PrismaService) {
+    super(prismaService);
   }
 
   async findManyByMovieIds(ids: string[]): Promise<{ movieId: string; genres: GenreModel[] }[]> {
