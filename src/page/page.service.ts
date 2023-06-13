25be5160c24fc89a0025410e174948e6bcffe646
mdwit <mdwit0r@gmail.com>
2023-06-13 21:59:46 +0200

    refactor(page.service.ts): extend BaseService class and remove unused imports and methods

--- src/page/page.service.ts
+++ src/page/page.service.ts
@@ -1,54 +1,23 @@
-import { findManyCursorConnection } from '@devoxa/prisma-relay-cursor-connection';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'nestjs-prisma';
-import { PaginationArgs } from 'src/common/pagination/pagination.args';
 import { FindPagesInput } from './dto/find-pages.input';
 import { PageModel } from './models/page.model';
 import { PagesModel } from './models/pages.model';
 import { PageMapper } from './page.mapper';
 import { CreatePageInput } from './dto/create-page.input';
+import { BaseService } from '../common/services/base.service';
 
 @Injectable()
-export class PageService {
-  constructor(private readonly prismaService: PrismaService) {}
-
-  async findOne(id: string): Promise<PageModel> {
-    const page = await this.prismaService.page.findUnique({
-      where: { id },
-    });
-
-    return new PageMapper().mapEntityToModel(page);
-  }
-
-  async findMany({ after, before, first, last }: PaginationArgs, where: FindPagesInput): Promise<PagesModel> {
-    const res = await findManyCursorConnection(
-      (args) => this.prismaService.page.findMany({ where }),
-      () => this.prismaService.page.count({ where }),
-      { after, before, first, last },
-    );
-
-    return {
-      ...res,
-      edges: res.edges.map((edge) => ({
-        ...edge,
-        node: new PageMapper().mapEntityToModel(edge.node),
-      })),
-    };
-  }
-
-  async create(data: CreatePageInput): Promise<PageModel> {
-    const page = await this.prismaService.page.create({
-      data: {
-        ...data,
-        pageInfo: {
-          create: {
-            description: null,
-            title: null,
-          },
-        },
-      },
-    });
-
-    return new PageMapper().mapEntityToModel(page);
+export class PageService extends BaseService(
+  'page',
+  PageModel,
+  PageModel,
+  PagesModel,
+  FindPagesInput,
+  CreatePageInput,
+  PageMapper,
+) {
+  constructor(readonly prismaService: PrismaService) {
+    super(prismaService);
   }
 }
