cf64b75b13f68fc4f4e724edab2ccdc2ddfe2189
mdwit <mdwit0r@gmail.com>
2023-06-18 02:20:53 +0200

    feat(main.ts): add cors configuration to enableCors method with allowed origins and credentials set to true

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,17 @@ async function bootstrap() {
   const corsConfig = configService.get<CorsConfig>('cors');
 
   if (corsConfig.enabled) {
-    app.enableCors();
+    app.enableCors({
+      origin: [
+        'http://localhost:3001',
+        'http://localhost:3000',
+        'https://dev-admin.openmoviedb.com',
+        'https://dev.openmoviedb.com',
+        'https://openmoviedb.com',
+        'https://admin.openmoviedb.com',
+      ],
+      credentials: true,
+    });
   }
 
   const config = new DocumentBuilder().setTitle('OpenMovieDB rest api').addBearerAuth().setVersion('1').build();
