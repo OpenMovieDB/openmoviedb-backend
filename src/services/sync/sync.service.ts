d636d345645f78be538fc823d63957bd58e2d27a
mdwit <mdwit0r@gmail.com>
2023-06-28 18:04:25 +0200

    feat(sync.service.ts): add getMovies method to SyncService to fetch movies from KinopoiskDev API
    The `getMovies` method is added to the `SyncService` class. This method uses the `KinopoiskDev` client to fetch movies from the KinopoiskDev API. The method accepts an optional `page` parameter to specify the page number of the movies to fetch. The method uses the `MovieQueryBuilder` to build a query for sorting movies by ID in descending order and paginating the results. The `MOVIES_LIMIT` constant is set to 250 to limit the number of movies fetched per page. The `KP_API_KEY` is retrieved from the `ConfigService` to initialize the `KinopoiskDev` client.

--- src/services/sync/sync.service.ts
+++ src/services/sync/sync.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { KinopoiskDev, MovieQueryBuilder, SORT_TYPE } from '@openmoviedb/kinopoiskdev_client';
 
 @Injectable()
-export class SyncService {}
+export class SyncService {
+  private readonly MOVIES_LIMIT = 250;
+  private kp: KinopoiskDev;
+
+  constructor(private readonly configService: ConfigService) {
+    this.kp = new KinopoiskDev(this.configService.get('KP_API_KEY'));
+  }
+
+  async getMovies(page: number = 1) {
+    const qb = new MovieQueryBuilder();
+    const query = qb.sort('id', SORT_TYPE.DESC).paginate(page, this.MOVIES_LIMIT).build();
+    return this.kp.movie.getByFilters(query);
+  }
+}
