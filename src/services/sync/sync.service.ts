86d1960675123cf4b52becff04c05e57aac4f50d
mdwit <mdwit0r@gmail.com>
2023-07-05 17:54:46 +0200

    feat(sync.service.ts): add logging for movie names during movie synchronization
    fix(sync.service.ts): handle error and throw an error if there is an error during movie synchronization

--- src/services/sync/sync.service.ts
+++ src/services/sync/sync.service.ts
@@ -1,14 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { KinopoiskDev, MovieQueryBuilder, SORT_TYPE } from '@openmoviedb/kinopoiskdev_client';
 
 @Injectable()
 export class SyncService {
+  private readonly logger = new Logger(SyncService.name);
   private readonly MOVIES_LIMIT = 250;
   private kp: KinopoiskDev;
 
   constructor(private readonly configService: ConfigService) {
     this.kp = new KinopoiskDev(this.configService.get('KP_API_KEY'));
+
+    this.syncMovies();
+  }
+
+  async syncMovies() {
+    const year = new Date().getFullYear();
+    const res = await this.getMoviesByYears(year);
+    if (res.error) {
+      this.logger.error(res.error);
+      throw new Error(res.error);
+    }
+
+    const { pages } = res.data;
+
+    for (let i = 1; i <= pages; i++) {
+      const res = await this.getMoviesByYears(year, i);
+      if (res.error) {
+        this.logger.error(res.error);
+        throw new Error(res.error);
+      }
+
+      const { data } = res;
+      for (const movie of data.docs) {
+        this.logger.log(movie.name);
+      }
+    }
   }
 
   async getMoviesByYears(year: number, page: number = 1) {
